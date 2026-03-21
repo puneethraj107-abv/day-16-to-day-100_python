245efa65c8cb74a06b1df594eb690b9753c5f02f
Puneeth R <puneethraj107@gmail.com>
2026-03-21 16:55:09 +0530

    learned to use OOP for the first time using documentation to build a coffee machine

--- Day-16/main.py
+++ Day-16/main.py
@@ -1,1 +1,28 @@
-print("hello world")
+from random import choice
+
+from menu import Menu,MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+coffee_maker=CoffeeMaker()
+menu=Menu()
+money_machine=MoneyMachine()
+
+
+machine_operation=True
+
+while machine_operation:
+    print("\nwelcome to the coffee maker machine")
+    options=menu.get_items()
+    choice=input(f"what would you like:({options}) ")
+    if choice=='off':
+        machine_operation=False
+    elif choice=='report':
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink=menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
