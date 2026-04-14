da7814d6f6faa22681b144746c3b74ed4459dd65
Puneeth R <puneethraj107@gmail.com>
2026-04-14 17:14:06 +0530

    completed the snake game with all its features. learnt about class, instances, list slicing

--- Day-20/main.py
+++ Day-20/main.py
@@ -7,9 +7,11 @@
 #TODO: 6. detect collision with wall
 #TODO: 7. detect collision with tail
 
-from turtle import Turtle,Screen
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 screen=Screen()
 screen.setup(width=600,height=600)
@@ -19,6 +21,9 @@ screen.tracer(0)
 
 
 snake = Snake()
+food=Food()
+scoreboard=Scoreboard()
+
 screen.listen()
 screen.onkey(snake.up,'Up')
 screen.onkey(snake.down,'Down')
@@ -34,4 +39,20 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-screen.exitonclick()
+    #TODO 4. detect collision with food
+    if snake.head.distance(food)<15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase()
+    #TODO: 6. detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on=False
+        scoreboard.game_over()
+    # TODO: 7. detect collision with tail
+    for segment in snake.segments[1:]:
+
+        if snake.head.distance(segment)<10:
+            game_is_on=False
+            scoreboard.game_over()
+
+
